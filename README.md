ec3c75111ff723fc6435d48d0dc0cc31cc0ddd0c
Ayan <imluckyayan@gmail.com>
2024-01-03 10:06:27 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# laser_slides
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/xkaper/laser_slides/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xkaper.laser_slides
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
