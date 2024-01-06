d1643558907e9af20a4be20bdd7af3048af668d5
Ayan <imluckyayan@gmail.com>
2024-01-07 00:58:45 +0530

    Defined app color palette

---
+++ lib/utils/palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const limeYellow = Color.fromRGBO(255, 239, 140, 1);
+  static const grey = Color.fromRGBO(173, 197, 195, 1);
+  static const violet = Color.fromRGBO(156, 116, 252, 1);
+  static const blue = Color.fromRGBO(136, 209, 240, 1);
+  static const pink = Color.fromRGBO(255, 163, 216, 1);
+  static const green = Color.fromRGBO(120, 220, 152, 1);
+}
+
+const paletteList = [
+  Palette.limeYellow,
+  Palette.blue,
+  Palette.green,
+  Palette.grey,
+  Palette.pink,
+  Palette.violet
+];
