b1aa126ecfdfa0991128403a5de273ed41db0d5c
Ayan <imluckyayan@gmail.com>
2024-01-07 00:59:47 +0530

    Update widgetsCatalogueBottomSheet function

--- lib/features/widgetCatalogue/widgets_catalogue.dart
+++ lib/features/widgetCatalogue/widgets_catalogue.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void widgetsCatalogueBottomSheet(BuildContext context, TickerProvider vsync) {
   showModalBottomSheet(
-    context: context,
-    transitionAnimationController: AnimationController(
-      vsync: vsync,
-      duration: const Duration(seconds: 1),
-    ),
-    builder: (context) {
-      return StaggeredGrid.extent(
-        maxCrossAxisExtent: 3,
-        children: const [],
-      );
-    },
-  );
+      context: context,
+      showDragHandle: true,
+      transitionAnimationController: AnimationController(
+        vsync: vsync,
+        duration: const Duration(seconds: 1),
+      ),
+      builder: (context) {
+        return const Text('Widgets Catalogue ');
+      });
 }
